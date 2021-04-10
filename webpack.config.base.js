03deae6a959631b450c26749acb818e5e9e1c5aa
clouddawn <1993456181@qq.com>
2021-04-10 20:14:12 +0800

    准备上上传

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,5 +13,47 @@ module.exports = {
     template: 'src/index.html'
   })
 ],
+module: {
+  rules: [
+    {
+      test: /\.(png|jpe?g|gif)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+        },
+      ],
+    },
+    {
+      test: /\.styl$/i,
+      loader: [
+        "style-loader",
+        "css-loader",
+        "stylus-loader",
+      ],
+    },
+    {
+      test: /\.less$/i,
+      loader: [
+        "style-loader",
+        "css-loader",
+        "less-loader",
+      ],
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader:'sass-loader',
+          options: {
+            implementation: require('dart-sass')
+          }
+        }
+      ],
+    },
+  ],
+},
 };
+
 
